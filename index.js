cd1c86e7bc90be55a8286c3eb8473d225e4619cf
Suruchi Kumari <suruchikumarimfp4@gmail.com>
2022-07-16 21:52:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+prompt("What is your name?");
+prompt("What is your crush name?");
+var n=(Math.random()*100);
+n=Math.floor(n)+1;
+if(n<30){
+  alert("Your ❤️ is like oil and water and your love is : "+n+"% true! Try to spend more time with your crush!");
+}
+else if(n>=30 && n<=70){
+  alert("Your ❤️ is : "+n+" % true ! Your crush also feels something for you but unable to express it ! Your love needs some time!")
+}
+else{
+  alert("Your ❤️ is : "+n+"% true . You both are like honey and water and you both are made for each other! ")
+}
